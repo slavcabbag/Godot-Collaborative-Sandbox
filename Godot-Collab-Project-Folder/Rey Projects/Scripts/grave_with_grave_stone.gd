7453967eb17bd04591538841ab41c4ba116282ed
SlavCabbag/Rey <rreeyygg@outlook.com>
2024-06-23 14:33:18 -0700

    Changed Graves to be random
    Picks a random gravestone at start and deletes all the others so no collision stays, I previously only used visibility but the collisions were overlapped + caused lag.

---
+++ Godot-Collab-Project-Folder/Rey Projects/Scripts/grave_with_grave_stone.gd
@@ -0,0 +1,20 @@
+extends Node3D
+
+@onready var gravestone_roof_2 = $"gravestone-roof2"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Get_Graves()
+
+func Get_Graves():
+	var graves = get_children()
+	# Would sometimes pick the grave as the random and have no gravestone
+	# Removing it from the list makes it so it doesn't get picked
+	
+	var picked_grave = graves.pick_random()
+	picked_grave.visible = true
+	for grave in graves:
+		if grave != picked_grave && grave.name != "grave2":
+			print(grave.name)
+			grave.queue_free()
+	

256ff5d00870026417cd682dcc32fc2b04e24535
SlavCabbag/Rey <rreeyygg@outlook.com>
2024-06-24 21:39:49 -0700

    added things to world and fence mapping and coloring
    I want to change the headbob to where it goes back to center if not moving

--- Godot-Collab-Project-Folder/Rey Projects/Scripts/Game Manager.gd
+++ Godot-Collab-Project-Folder/Rey Projects/Scripts/Game Manager.gd
@@ -5,13 +5,14 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	# exit out of game
 	if Input.is_action_just_pressed("Menu-Escape"):
 		queue_free()
 		get_tree().quit()
-func _process(delta):
+func _process(_delta):
 	var fps = Engine.get_frames_per_second()
 	label_2.text = "FPS: " + str(fps)
 

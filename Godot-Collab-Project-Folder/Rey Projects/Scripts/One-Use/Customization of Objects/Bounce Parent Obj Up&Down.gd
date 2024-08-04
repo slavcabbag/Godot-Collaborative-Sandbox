3783fa1a8745f173e8516cae2a6748abbeeef5e2
slavcabbag <92615930+slavcabbag@users.noreply.github.com>
2024-08-03 19:19:20 -0700

    funny grass, hold 6 to reverse

--- Godot-Collab-Project-Folder/Rey Projects/Scripts/One-Use/Customization of Objects/Bounce Parent Obj Up&Down.gd
+++ Godot-Collab-Project-Folder/Rey Projects/Scripts/One-Use/Customization of Objects/Bounce Parent Obj Up&Down.gd
@@ -4,34 +4,40 @@ var randnum = 0
 var up_ = true
 var scale__h
 var doOnce = true
-
+var switching_direction = false
+var start_script = false
+var numnum = 1
+@onready var timer = $Timer
 
 func _process(delta):
+	
 	if doOnce:
-		randnum = randi_range(1,1) 
+		randnum = randi_range(1,6) 
 		if str(get_parent().get_script()) == "<GDScript#-9223295497769226097>":
-			scale__h = get_parent().scale_h	
+			scale__h = get_parent().scale_h
 		doOnce = false
-		
+		timer.start()
+	if start_script == false && Engine.get_frames_drawn() % randnum == 0.0: 
+		start_script = true
+	
+	if Input.is_key_pressed(KEY_6):
+		numnum = 1 
+	else: 
+		numnum = -1
+	
 	if str(get_parent().get_script()) == "<GDScript#-9223295497769226097>":
-		print(get_parent().name)
 		pass
-		#if Engine.get_frames_drawn() % randnum == 0.0:
-			#if up_ && get_parent().scale_h > 3.4: up_ = false
-			#if !up_ && get_parent().scale_h < 3: up_ = true
-			#
-			#if up_:
-				#get_parent().scale_h = lerp(get_parent().scale_h, 5.5, delta * 5)
-				#
-			#else:
-				#get_parent().scale_h = lerp(get_parent().scale_h, 1.5, delta * 5)
 	else:
-		if Engine.get_frames_drawn() % randnum == 0.0:
-			if up_ && get_parent().scale.y > 2: up_ = false
-			if !up_ && get_parent().scale.y < 1: up_ = true
-			
-			if up_:
-				get_parent().scale.y = lerp(get_parent().scale.y, 2.1, delta * 5)
+		if start_script == true:
+			if up_ && get_parent().scale.y > 2: 
+				up_ = false
+				timer.start()
+			if !up_ && get_parent().scale.y < 0.5: 
+				up_ = true
+				timer.start()
 				
-			else:
-				get_parent().scale.y = lerp(get_parent().scale.y, 0.9, delta * 5)
+			if timer.time_left == 0:
+				if up_:
+					get_parent().scale.y = lerp(get_parent().scale.y, 2.05, delta * numnum)
+				if !up_:
+					get_parent().scale.y = lerp(get_parent().scale.y, 0.45, delta * numnum)

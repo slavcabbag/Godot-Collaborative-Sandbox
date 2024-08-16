2f3841df6861206bc2b79557b5ea3ecf8b382440
slavcabbag <92615930+slavcabbag@users.noreply.github.com>
2024-08-15 23:41:18 -0700

    Zombi bro moving away issue

--- Godot-Collab-Project-Folder/Rey Projects/Scripts/ZombiBro.gd
+++ Godot-Collab-Project-Folder/Rey Projects/Scripts/ZombiBro.gd
@@ -1,2 +1,24 @@
 extends CharacterBody3D
 
+
+var player = null
+
+const SPEED = 4.0
+
+@export var player_path : NodePath
+
+@onready var nav_agent = $NavigationAgent3D
+
+
+func _ready():
+	player = get_node(player_path)
+	print(str(player))
+func _process(delta):
+	velocity = Vector3.ZERO
+	nav_agent.set_target_position(player.global_transform.origin)
+	var next_nav_point = nav_agent.get_next_path_position()
+	velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
+	
+	look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP)
+	
+	move_and_slide()

e2d42c0089ea85e034961db2ca0409d6041ed1ee
slavcabbag <92615930+slavcabbag@users.noreply.github.com>
2024-09-30 21:34:18 -0700

    Zombies have boomboxes now
    Also had to reset the zombie bit and it was really easy. All I did was discard changes on zombie, fetch origin, and boom reset nothing else but the zombro cool, convienient

--- Godot-Collab-Project-Folder/Rey Projects/Scripts/bullet.gd
+++ Godot-Collab-Project-Folder/Rey Projects/Scripts/bullet.gd
@@ -22,10 +22,8 @@ func _process(delta):
 		var collider = ray.get_collider() 
 		mesh.visible = false
 		particles.emitting = true
-		print(str(collider.name))
 		ray.enabled = false		
 		if ray.get_collider().is_in_group("enemy"):
-			print("true5")
 			ray.get_collider().hit()
 
 		await get_tree().create_timer(1.0).timeout
